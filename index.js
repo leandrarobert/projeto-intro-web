3683f8ce7793a0d074946ea2b299904e7f4d6aa4
leandrarobert <leandraroberta234@gmail.com>
2022-10-29 21:59:35 -0300

    projeto inro web leandra

---
+++ index.js
@@ -0,0 +1,253 @@
+// //  nomedaserie = ; 
+// //  numerodetemporada =;
+// //  adaptacao = ; 
+// // personagensprincipais;
+
+// primeira semana:
+// const nomedaserie1 = String("Supernatural")
+// const numerodetemporada1 = Number(15)
+// const adaptacao1 = Boolean(false)
+// const personagensprincipais1 = ["Dean Winchester", "Sam Winchester"]
+
+// const nomedaserie2 = String("Arrow")
+// const numerodetemporada2 = Number(8)
+// const adaptacao2 = Boolean(true)
+// const personagensprincipais2 = ["Oliver Queen", "Felicity Smoak", "John Diggle"]
+
+
+// const nomedaserie3 = String("Brooklyn-99")
+// const numerodetemporada3 = Number(8)
+// const adaptacao3 = Boolean(false)
+// const personagensprincipais3 = ["Jake Peralta"]
+
+
+// let soma = (numerodetemporada1 + numerodetemporada2 + numerodetemporada3)
+// const media = soma/3
+// console.log(media)
+
+// const adaptacao = adaptacao1 && adaptacao2 && adaptacao3
+// console.log(adaptacao)
+
+// console.log(nomedaserie1.toUpperCase() +  "\n" + "Número de temporadas:" + "\n"  + numerodetemporada1 + "\n" + "É adaptação?" + "\n"  + adaptacao1 + "\n"  + "Personagens Principais:" + "\n" +  personagensprincipais1)
+
+// console.log(nomedaserie2.toUpperCase() +  "\n" + "Número de temporadas:" + "\n"  + numerodetemporada2 + "\n" + "É adaptação?" + "\n"  + adaptacao2 + "\n"  + "Personagens Principais:" + "\n" +  personagensprincipais2)
+
+// console.log(nomedaserie3.toUpperCase() +  "\n" + "Número de temporadas:" + "\n"  + numerodetemporada3 + "\n" + "É adaptação?" + "\n"  + adaptacao3 + "\n"  + "Personagens Principais:" + "\n" +  personagensprincipais3)
+
+// let soma = (numerodetemporada1 + numerodetemporada2 + numerodetemporada3)
+//  media = soma/3
+// console.log(media)
+
+//  adaptacao = adaptacao1 && adaptacao2 && adaptacao3
+// console.log(adaptacao)
+
+// console.log(nomedaserie1.toUpperCase() +
+//   "\n" + "Número de temporadas:" + 
+  
+//   "\n"  + numerodetemporada1 + "\n" +
+//    "É adaptação?" + "\n"  + adaptacao1 + "\n"  +
+//      "Personagens Principais:" + "\n" +  personagensprincipais1)
+
+// console.log(nomedaserie2.toUpperCase() +  "\n" + 
+// "Número de temporadas:" + "\n"  + numerodetemporada2 +
+//  "\n" + "É adaptação?" + "\n"  + adaptacao2 +
+//   "\n"  + "Personagens Principais:" + "\n" +  personagensprincipais2)
+
+// console.log(nomedaserie3.toUpperCase() +  "\n" + 
+// "Número de temporadas:" + "\n"  + numerodetemporada3 + "\n" +
+//  "É adaptação?" + "\n"  + adaptacao3 + "\n"  + 
+//  "Personagens Principais:" + "\n" +  personagensprincipais3)
+
+// // 2 semana
+
+// const array = []
+
+// let objeto1 = {
+//     nomedaserie1:"Supernatural",
+//     numerodetemporada1: 15,
+//     adaptacao1: false,
+//     personagensprincipais1: ["Dean Winchester", "Sam Winchester"]
+// }
+//  let objeto2 = {
+//     nomedaserie2:"Arrow",
+//      numerodetemporada2: 8,
+//      adaptacao2: true,
+//      personagensprincipais2: ["Oliver Queen", "Felicity Smoak", "John Diggle"]
+    
+// }
+// let objeto3 = {
+//      nomedaserie3: "Brooklyn-99",
+//      numerodetemporada3: 8,
+//      adaptacao3: false,
+//      personagensprincipais3: ["Jake Peralta"]
+// }
+
+
+// if (objeto1.adaptacao1 === true ) {
+//      array.push(objeto1)
+// } else {
+//      console.log(alert("Não é adaptação"))
+// }
+
+// if (objeto2.adaptacao2 === true ) {
+//      array.push(objeto2)
+// } else {
+//      console.log(alert("Não é adaptação"))
+// }
+
+// if (objeto3.adaptacao3 === true ) {
+//      array.push(objeto3)
+// } else {
+//      console.log(alert("Não é adaptação"))
+// }
+
+// console.log(array)
+
+// semana 5
+//  <header>
+//         <h1>Series</h1>
+//     </header>
+//     <div>
+//          <input type="text">  
+//     <button type="button">Buscar</button>
+//     </div>
+   
+//     <section>
+        
+//         <ul>
+//             <li id="seriesupernatural">nome da serie:<a href="https://www.warnerbros.com/tv/supernatural">Supernatural</a></li> 
+//             <li id="tempsupernatural">numero de temporadas: 15</li>
+//             <li id="adapsupernatural">adaptacao: false</li>
+//             <li id="personagensupernatural">personagens principais1: Dean Winchester, Sam Winchester</li>
+//         </ul>
+//          <ul>
+//             <li>nome da serie:<a href="https://www.lojadccomics.com.br/series/arrow">Arrow</a> </li>
+//             <li>numero de temporadas: 8</li>
+//             <li>adaptacao: true</li>
+//             <li>personagens principais1: Oliver Queen, Felicity Smoak, John Diggle</li>
+            
+//         </ul>
+//         <ul>
+//             <li>nome da serie:<a href="https://theninenine.com/">Brooklyn-99</a> </li>
+//             <li>numero de temporadas: 8</li>
+//             <li>adaptacao: false</li>
+//             <li>personagens principais1: Jake Peralta</li>
+            
+//         </ul>
+//         <p></p>
+//     </section>
+//     <footer>
+//         <h1>Olá, eu sou o Footer</h1>
+//     </footer>
+    
+// </body>
+// </html>
+
+// 3 semana
+
+
+// nomedaserie1 = String("Supernatural")
+// numerodetemporada1 = Number(15)
+// adaptacao1 = false
+// personagensprincipais1 = "Dean Winchester, Sam Winchester"
+
+// nomedaserie2 = String("Arrow")
+// numerodetemporada2 = Number(8)
+// adaptacao2 = true
+// personagensprincipais2 = "Oliver Queen, Felicity Smoak, John Diggle"
+
+
+// nomedaserie3 = String("Brooklyn-99")
+// numerodetemporada3 = Number(8)
+// adaptacao3 = false
+// const personagensprincipais3 = "Jake Peralta"
+
+
+// console.log(nomedaserie1.toUpperCase() +
+//   "\n" + "Número de temporadas:" +
+
+//   "\n" + numerodetemporada1 + "\n" +
+//   "É adaptação?" + "\n" + adaptacao1 + "\n" +
+//   "Personagens Principais:" + "\n" + personagensprincipais1)
+
+// console.log(nomedaserie2.toUpperCase() + "\n" +
+//   "Número de temporadas:" + "\n" + numerodetemporada2 +
+//   "\n" + "É adaptação?" + "\n" + adaptacao2 +
+//   "\n" + "Personagens Principais:" + "\n" + personagensprincipais2)
+
+// console.log(nomedaserie3.toUpperCase() + "\n" +
+//   "Número de temporadas:" + "\n" + numerodetemporada3 + "\n" +
+//   "É adaptação?" + "\n" + adaptacao3 + "\n" +
+//   "Personagens Principais:" + "\n" + personagensprincipais3)
+
+const series = [
+    {
+      nomedaserie: "Supernatural",
+      numerodetemporada: 15,
+      adaptacao: false,
+      personagensprincipais: "Dean Winchester, Sam Winchester"
+    }, {
+      nomedaserie: "Arrow",
+      numerodetemporada: 8,
+      adaptacao: true,
+      personagensprincipais: "Oliver Queen, Felicity Smoak, John Diggle"
+    }, {
+      nomedaserie: "Brooklyn-99",
+      numerodetemporada: 8,
+      adaptacao: false,
+      personagensprincipais: "Jake Peralta"
+    },
+  
+  
+  ]
+  
+  for(seriado of series){
+  
+    for (propriedade in seriado){
+  
+        console.log(`${propriedade}: ${seriado[propriedade]}`)
+  
+    }
+  
+  console.log("----------------------");
+  
+  }
+  
+  function imprimestring(arrayobjeto, texto) {
+    for (serie of arrayobjeto) {
+      if (serie.nomedaserie === texto) {
+        console.log(serie)
+        return (serie) 
+      } 
+    } //alert("Não achou a série")
+   
+  }
+  
+  
+  imprimestring(series, "" )
+  
+  
+  
+  
+  const linksupernatural = document.getElementById("linksupernatural").innerHTML = `${series[0].nomedaserie}`;
+  const tempsupernatural = document.getElementById("tempsupernatural").innerHTML = "Numero de temporadas:" + `${series[0].numerodetemporada}`;
+  const adapsupernatural = document.getElementById("adapsupernatural").innerHTML = "Adaptação:" + `${series[0].adaptacao}`;
+  const personagenssupernatural = document.getElementById("personagensupernatural").innerHTML = `${series[0].personagensprincipais}`;
+  
+  const linkarrow= document.getElementById("linkarrow").innerHTML = `${series[1].nomedaserie}`;
+  const temparrow = document.getElementById("temparrow").innerHTML = "Numero de temporadas:" + `${series[1].numerodetemporada}`;
+  const adaparrow = document.getElementById("adaparrow").innerHTML = "Adaptação:" + `${series[1].adaptacao}`;
+  const personagensarrow = document.getElementById("personagensarrow").innerHTML = `${series[1].personagensprincipais}`;
+  
+  const linkbrooklyn= document.getElementById("linkbrookly").innerHTML = `${series[2].nomedaserie}`;
+  const tempbrooklyn = document.getElementById("tempbrooklyn").innerHTML = "Numero de temporadas:" + `${series[2].numerodetemporada}`;
+  const adapbrooklyn = document.getElementById("adapbrooklyn").innerHTML = "Adaptação:" + `${series[2].adaptacao}`;
+  const personagensbrooklyn = document.getElementById("personagensbrooklyn").innerHTML = `${series[2].personagensprincipais}`;
+  
+  
+  if(!series[0].serie.toLowerCase().includes(inputBusca.value.toLowerCase())){      
+       section1classList.add("invisible")    
+       } else {         section1.classList.remove("invisible")     }
+       
+  
+  
